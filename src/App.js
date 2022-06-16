73a5c1c96893bbae783edd9162d9e22bc5fb1216
Debaditya <ddev2636@gmail.com>
2022-06-17 00:12:02 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
+import Footer from "./components/footer/Footer";
 import Stats from "./components/statistics/Stats";
 
 function App() {
   return <div>
     <Stats/>
+    <Footer/>
   </div>;
 }
 
