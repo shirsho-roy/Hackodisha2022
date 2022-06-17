11f193310085fe72dd6371ce3218fd7b04d98859
Aashirbad Sabat <aum.aashirbad@gmail.com>
2022-06-17 11:57:05 +0530

    Featues added in About Us

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-
+import About from "./components/About/AboutUs";
 import Footer from "./components/footer/Footer";
 import Stats from "./components/statistics/Stats";
 
 function App() {
-  return <div>
-    <Stats/>
-    <Footer/>
-  </div>;
+  return (
+    <div>
+      <About />
+      <Stats />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
