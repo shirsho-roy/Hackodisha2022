ea071464e4ca64fc4ab9140d18aa171118332766
Debaditya <ddev2636@gmail.com>
2022-07-10 13:54:17 +0530

    tracks-timeline-prizes

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Timeline = () => {
   return (
-    <div className="bg-gray-900 h-100px text-white pl-28 p-4">
+    <div className="bg-gray-900 h-100px text-white pl-5 md:pl-28 sm:pl-9 pt-4 pb-4 ">
         <div className='text-[44px]'>Timeline</div>
         <div className='text-[20px] pl-1 '>Updating soon...</div>
     </div>
