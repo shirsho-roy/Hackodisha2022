8e3ae5690d33a3a7ca2add0d9c8d6820de3f1c09
Manjeet Dhayal <76167344+manjeetdhayal@users.noreply.github.com>
2022-07-12 10:54:41 +0530

    bg update

--- src/components/Prizes/Prizes.js
+++ src/components/Prizes/Prizes.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Prizes = () => {
   return (
-    <div className="bg-gray-900 h-100px text-white pl-5 md:pl-28 sm:pl-9  p-4">
+    <div className="bg-[#000000] h-100px text-white pl-5 md:pl-28 sm:pl-9  p-4">
         <div className='text-[44px]'>Prizes</div>
         <div className='text-[20px] pl-1 '>Updating soon...</div>
     </div>
