a825d103702428524320c03dd404999f1b9180dc
Manjeet Dhayal <76167344+manjeetdhayal@users.noreply.github.com>
2022-07-12 10:56:02 +0530

    bg update

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Track = () => {
   return (
-    <div className="bg-gray-900 h-100px text-white pl-5 sm:pl-9 md:pl-28 p-4">
+    <div className="bg-[#000000] h-100px text-white pl-5 sm:pl-9 md:pl-28 p-4">
         <div className='text-[44px]'>Tracks</div>
         <div className='text-[20px] pl-1 '>Updating soon...</div>
     </div>
