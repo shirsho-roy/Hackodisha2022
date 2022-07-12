24b242807ee4cf066369ee0803e1b7b9f89bc511
Aashirbad Sabat <aum.aashirbad@gmail.com>
2022-07-12 11:19:01 +0530

    Updated content of AboutUs

--- src/components/About/AboutUs.js
+++ src/components/About/AboutUs.js
@@ -15,10 +15,21 @@ export default function About() {
         <div className="border-[3px] border-white rounded-2xl  h-[20vh] sm:h-[40vh] xl:h-[40vh] mb-[10%] w-[70%] lg:w-[40%] xl:w-[35%] mx-[10%] lg:ml-36 lg:mr-20"></div>
         <div className=" px-[10%] lg:pl-[2%] lg:pr-0 mt-[5%] lg:mt-[1%] lg:w-[50%] lg:mr-2 ">
           <p className="font-sans lg:w-[90%]  text-sm md:text-xl lg:text-3xl  text-white ">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            Welcome to HackOdisha 2.0, a hackathon that aims to bring creatives
+            and developers together to solve some of the most pressing problems
+            faced by communities all over the world. This 36-hour-long event
+            will bring together technocrats from all over the country. The
+            problem statements are stimulating enough for programmers who love
+            solving mind-bending problems. We look forward to connecting with
+            the participants and hope they enjoy their journey to this Eden of
+            coding. After the success of the first season of HackOdisha, Webwiz
+            is proud to host the greater-than-ever HackOdisha 2.0 Webwiz is not
+            just any other technical club. Our mission is to join creative minds
+            with technical skills to come up with innovative ways of using
+            sponsors’ technologies to develop innovative solutions to solve
+            important real-world problems. Therefore, the hackathon is open to
+            experts as well as beginners because abstractions are equally
+            important as implementations.
           </p>
           <button
             type="button"
