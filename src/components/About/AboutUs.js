3d486730257dd7b4f159f84dc29eb4f1e0740adb
shirsho-roy <shirsho.tffs@gmail.com>
2022-07-13 21:39:20 +0530

    Fonts editted

--- src/components/About/AboutUs.js
+++ src/components/About/AboutUs.js
@@ -3,19 +3,19 @@ import "./AboutUs.css";
 
 export default function About() {
   return (
-    <section id="About">
+    <section id="About ">
     <div className="AboutUs container w-[100%]">
-      <div className=" text-3xl lg:text-5xl ml-[5%] lg:ml-[10%] xl:ml-[7%] pt-20 mb-12 text-white tracking-normal font-medium">
+      {/* <div className=" text-3xl lg:text-5xl ml-[5%] lg:ml-[10%] xl:ml-[7%] pt-20 mb-12 text-white tracking-normal font-medium">
         About Us
-      </div>
+      </div> */}
       <div className="lg:flex pb-4 ">
         <img
           src="https://res.cloudinary.com/ash123/image/upload/v1657686843/HackOdisha2022/AboutUsImg_jlahdm-removebg-preview_fttq1j.png"
           alt="HackOdisha2022"
-          className="h-[20vh] sm:h-[35vh] xl:h-[45vh] mb-[10%] w-[70%] lg:w-[50%] xl:w-[35%] mx-[10%] lg:ml-36 lg:mr-12"
+          className="h-[20vh] sm:h-[35vh] xl:h-[45vh] mb-[10%] w-[50%] lg:w-[55%] xl:w-[42%] mx-[10%] lg:ml-36 lg:mr-12"
         />
-        <div className=" px-[10%] lg:pl-[2%] lg:pr-0 lg:w-[50%] lg:mr-2 ">
-          <p className="font-sans lg:w-[90%]  text-sm md:text-xl lg:text-2xl text-white leading-4">
+        <div className=" px-[10%] lg:pl-[2%] lg:pr-0 lg:w-[60%] lg:mr-3 ">
+          <p className="font-sans lg:w-[95%]  text-sm md:text-base lg:text-xl txt leading-4">
             Welcome to HackOdisha 2.0, a hackathon that aims to bring creatives
             and developers together to solve some of the most pressing problems
             faced by communities all over the world. This 36-hour-long event
@@ -28,12 +28,12 @@ export default function About() {
             well as beginners because abstractions are equally important as
             implementations.
           </p>
-          <button
+          {/* <button
             type="button"
             class="text-white border-[3px] hover:bg-[#170d20] border-white font-medium rounded-3xl text-base lg:text-2xl px-3 py-0.5 lg:px-12 lg:py-2 text-center mr-2 my-12 "
           >
             Read More
-          </button>
+          </button> */}
         </div>
       </div>
     </div>
