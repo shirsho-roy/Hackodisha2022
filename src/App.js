88d3f2c5582347e3040e6830b4a6950444fe0189
manjeetdhayal <manjeetdhayal786@gmail.com>
2022-08-13 11:08:02 +0530

    Minor changes Done

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
         <Track />
         <Prizes />
         <Timeline />
-        {/* <SponserFinal /> */}
+        <SponserFinal />
         <FAQ />
         <Footer />
       </div>
