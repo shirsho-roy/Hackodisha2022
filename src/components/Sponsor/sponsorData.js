6be33236996c006d3b18020e16b887d2224587a0
shirsho-roy <shirsho.tffs@gmail.com>
2022-08-25 22:06:12 +0530

    Sponsors Updated

--- src/components/Sponsor/sponsorData.js
+++ src/components/Sponsor/sponsorData.js
@@ -1,13 +1,21 @@
 import MLH from "../../assets/Sponsors/Platinum/MLH.png";
 import NITR from "../../assets/Sponsors/Platinum/Nit.png";
+import Github from "../../assets/Sponsors/Platinum/github.png";
 
 import devfolio from "../../assets/Sponsors/Gold/devfolio.png";
 import github from "../../assets/Sponsors/Gold/Github.png";
 import polygon from "../../assets/Sponsors/Gold/polygon.png";
+import fueler from "../../assets/Sponsors/Gold/fueler.png";
 
 import Filecoin from "../../assets/Sponsors/Silver/Filecoin.png";
 import stream from "../../assets/Sponsors/Silver/Streamyard.png";
 import hoverr from "../../assets/Sponsors/Silver/hoverrobotix.png";
+import clerky from "../../assets/Sponsors/Silver/Clerky_Logo.jpg";
+import coil from "../../assets/Sponsors/Silver/coil.png";
+import godaddy from "../../assets/Sponsors/Silver/goDaddy.png";
+import superteam from "../../assets/Sponsors/Silver/superteam.png";
+import twilio from "../../assets/Sponsors/Silver/twilio.png";
+import xyz from "../../assets/Sponsors/Silver/xyz.png";
 
 import oneP from "../../assets/Sponsors/Bronze/1p.png";
 import axure from "../../assets/Sponsors/Bronze/axure.png";
@@ -19,7 +27,14 @@ import Interview from "../../assets/Sponsors/Bronze/interviewbuddy.png";
 import taskade from "../../assets/Sponsors/Bronze/taskade.png";
 import noscratch from "../../assets/Sponsors/Bronze/nostarch.png";
 import wolfram from "../../assets/Sponsors/Bronze/wolfram.png";
-
+import hedera from "../../assets/Sponsors/Bronze/hedera.png";
+import cake from "../../assets/Sponsors/Bronze/interview_cake.png";
+import jetbrains from "../../assets/Sponsors/Bronze/jet_brains.png";
+import rosefeld from "../../assets/Sponsors/Bronze/Rosefeld.png";
+import standoutstickers from "../../assets/Sponsors/Bronze/standOutStickers.png";
+import testmail from "../../assets/Sponsors/Bronze/testmail.png";
+import hackclub from  "../../assets/Sponsors/Bronze/hackClub.png";
+import snyk from "../../assets/Sponsors/Bronze/snyk.png";
 const platinumSponsors = [
   {
     id: 1,
@@ -32,6 +47,12 @@ const platinumSponsors = [
     link: "https://mlh.io/",
     image: MLH,
     alt: "Postman platinum sponsor of Hackodisha",
+  },
+  {
+    id: 3,
+    link: "https://github.com/",
+    image: Github,
+    alt: "Github gold sponsor for Hackodisha",
   },
 ];
 
@@ -54,6 +75,12 @@ const goldSponsors = [
     image: polygon,
     alt: "Polygon gold sponsor for Hackodisha",
   },
+  {
+    id: 4,
+    link: "https://fueler.io/",
+    image: fueler,
+    alt: "Fueler gold sponsor for Hackodisha",
+  },
 ];
 const silverSponsors = [
   {
@@ -74,6 +101,42 @@ const silverSponsors = [
     image: hoverr,
     alt: "Hover Robotix silver sponsor for Hackodisha",
   },
+  {
+    id: 4,
+    link: "https://www.clerky.com/?gclid=CjwKCAjwu5yYBhAjEiwAKXk_eLhU4ZaifbSS88phfu2yS9cUrwz8VYP3oVvwfz3ialZYAFw46fS8NBoCToAQAvD_BwE",
+    image: clerky,
+    alt: "Clerky Logo Robotix silver sponsor for Hackodisha",
+  },
+  {
+    id: 5,
+    link: "https://coil.com/",
+    image: coil,
+    alt: "Coil Robotix silver sponsor for Hackodisha",
+  },
+  {
+    id: 6,
+    link: "https://www.godaddy.com/en-in",
+    image: godaddy,
+    alt: "GoDaddy Robotix silver sponsor for Hackodisha",
+  },
+  {
+    id: 7,
+    link: "https://superteam.fun/",
+    image: superteam,
+    alt: "Super Team silver sponsor for Hackodisha",
+  },
+  {
+    id: 8,
+    link: "https://www.twilio.com/",
+    image: twilio,
+    alt: "Twilio silver sponsor for Hackodisha",
+  },
+  {
+    id: 9,
+    link: "https://gen.xyz/",
+    image: xyz,
+    alt: "xyz Robotix silver sponsor for Hackodisha",
+  },
 ];
 
 const bronzeSponsors = [
@@ -82,19 +145,21 @@ const bronzeSponsors = [
     link: "https://1password.com/",
     image: oneP,
     alt: "Echo AR bronze sponsor for Hackodisha",
+    style: { width: "120px" },
   },
   {
     id: 2,
     link: "https://www.axure.com/",
     image: axure,
     alt: "Axure",
-    style: { width: "150px" },
+    style: { width: "120px" },
   },
   {
     id: 3,
     link: "https://balsamiq.com/",
     image: balsamiq,
     alt: "Balsamiq bronze sponsor for Hackodisha",
+    style: { width: "120px" },
   },
   {
     id: 4,
@@ -108,6 +173,7 @@ const bronzeSponsors = [
     link: "https://www.echo3d.co/",
     image: echo,
     alt: "Echo 3D bronze sponsor for Hackodisha",
+    style: { width: "120px" },
   },
   {
     id: 6,
@@ -121,12 +187,14 @@ const bronzeSponsors = [
     link: "https://www.taskade.com/",
     image: taskade,
     alt: "Taskade bronze sponsor for Hackodisha",
+    style: { width: "120px" },
   },
   {
     id: 9,
     link: "https://noscratch.com/",
     image: noscratch,
     alt: "noscratch bronze sponsor for Hackodisha",
+    style: { width: "70px" },
   },
   {
     id: 10,
@@ -139,6 +207,55 @@ const bronzeSponsors = [
     link: "https://www.wolframalpha.com/",
     image: wolfram,
     alt: "Wolfram bronze sponsor for Hackodisha",
+  },
+  {
+    id: 12,
+    link: "https://hedera.com/",
+    image: hedera,
+    alt: "Hedera bronze sponsor for Hackodisha",
+    style: { width: "120px" },
+  },
+  {
+    id: 13,
+    link: "https://www.interviewcake.com/",
+    image: cake,
+    alt: "Interview Cake bronze sponsor for Hackodisha",
+  },
+  {
+    id: 14,
+    link: "https://www.jetbrains.com/",
+    image: jetbrains,
+    alt: "Jet Brains bronze sponsor for Hackodisha",
+  },
+  {
+    id: 15,
+    link: "https://rosenfeldmedia.com/",
+    image: rosefeld,
+    alt: "Rosefeld bronze sponsor for Hackodisha",
+  },
+  {
+    id: 16,
+    link: "https://www.standoutstickers.com/",
+    image:standoutstickers,
+    alt: "Stand Out Stickers bronze sponsor for Hackodisha",
+  },
+  {
+    id: 17,
+    link: "https://testmail.app/",
+    image: testmail,
+    alt: "Testmail bronze sponsor for Hackodisha",
+  },
+  {
+    id: 18,
+    link: "https://snyk.io/",
+    image: snyk,
+    alt: "Snyk bronze sponsor for Hackodisha",
+  },
+  {
+    id: 19,
+    link: "https://hackclub.com/",
+    image: hackclub,
+    alt: "HackClub bronze sponsor for Hackodisha",
   },
 ];
 
